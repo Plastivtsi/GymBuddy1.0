a12ad18444ffc1f1d93e6c177f671abbab4f3e9a
Donni-01 <danikm1029@gmail.com>
2025-03-24 17:09:23 +0200

    Add new Template

---
+++ DAL/Migrations/20250324150830_AddTemplate2.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTemplate2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Shablon",
+                table: "Exercises",
+                newName: "Template");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Template",
+                table: "Trainings",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Template",
+                table: "Trainings");
+
+            migrationBuilder.RenameColumn(
+                name: "Template",
+                table: "Exercises",
+                newName: "Shablon");
+        }
+    }
+}
--- DAL/Models/Training.cs
+++ DAL/Models/Training.cs
@@ -14,6 +14,7 @@ namespace DAL.Models
         public TimeSpan Time { get; set; }
         public string Description { get; set; }
         public int UserId { get; set; }
+        public bool Template { get; set; }
 
         // Навігаційні властивості
         public User User { get; set; }
