875460becf78ccac71a4562ece3e27f8bb01e1b9
andri <andri@DESKTOP-7RLFM1U>
2025-03-29 22:25:25 +0200

    some changes

--- PL/Controllers/ProfileController.cs
+++ PL/Controllers/ProfileController.cs
@@ -8,7 +8,6 @@ namespace YourProject.Controllers
 {
     public class ProfileController : Controller
     {
-        // Служба для доступу до даних користувача (можливо, вам потрібна інша реалізація)
         private readonly IUserService _userService;
 
         public ProfileController(IUserService userService)
@@ -19,14 +18,12 @@ namespace YourProject.Controllers
         // GET: /Profile/
         public IActionResult Index()
         {
-            // Отримуємо поточного користувача
-            // Тут може бути ваша логіка автентифікації
             var userId = Autorization.CurrentUserId;
-            var user = _userService.GetUserById(userId.ToString()); // Приклад методу
+            var user = _userService.GetUserById(userId.ToString()); 
 
             if (user == null)
             {
-                return RedirectToAction("Login", "Account"); // Якщо користувач не авторизований
+                return RedirectToAction("Login", "Account"); 
             }
 
             return View(user);
@@ -52,11 +49,11 @@ namespace YourProject.Controllers
         {
             if (ModelState.IsValid)
             {
-                _userService.UpdateUser(model); // Оновлюємо користувача
-                return RedirectToAction("Index"); // Перенаправляємо на профіль після редагування
+                _userService.UpdateUser(model); 
+                return RedirectToAction("Index"); 
             }
 
-            return View(model); // Повертаємо на сторінку редагування, якщо є помилки в моделі
+            return View(model); 
         }
     }
 }
